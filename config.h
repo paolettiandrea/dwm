386000b72221b4c0331b0dbf289c400bd3e76a41
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-11-27 13:51:19 +0100

    Changed status font to SourceCodePro of size 12

--- config.h
+++ config.h
@@ -7,8 +7,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "SourceCodePro:size=12" };
+static const char dmenufont[]       = "SourceCodePro:size=12";
 static const char col_gray1[]       = "#1d1f21";
 static const char col_gray2[]       = "#323233";  
 static const char col_gray3[]       = "#929593";
