a2fe563920eb7ef51756b77f9432557f31a4da79
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2022-10-19 18:53:58 +0200

    Added fibonacci layout

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 #include "patches/selfrestart.c"
+#include "patches/fibonacci.c"
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
@@ -40,9 +41,12 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
+
+	{ "[\\]",     dwindle },  /* first entry is default */
+	{ "[]=",      tile },
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+    { "[@]",      spiral },
 };
 
 /* key definitions */
@@ -59,11 +63,13 @@ static const Layout layouts[] = {
 #define TERM "alacritty"
 #define TERMCMD(cmd) { .v = (const char*[]){ TERM, "-e", cmd, NULL } }
 
+#define BROWSER "brave"
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { TERM, NULL };
-static const char *browsercmd[]  = { "brave", NULL };
+static const char *browsercmd[]  = { BROWSER, NULL };
 static const char *cwdtermcmd[]  = { "alacritty", "--working-directory", "`xcwd`", NULL };
 
 #define METACOMBO MODKEY|ShiftMask
@@ -82,14 +88,14 @@ static Key keys[] = {
 	
 	/* META ============================================================================= */
 	// General
-	{ METACOMBO,             				XK_b,      			togglebar,      {0} },                    /* toggle bar*/                          
+	{ METACOMBO,             		XK_b,      			togglebar,      {0} },                    /* toggle bar*/
 	{ MODKEY|ControlMask,           XK_q,      			quit,         	{0} },                    /* quit dwm */
 	{ MODKEY|ControlMask,           XK_r,      			self_restart,   {0} },            				/* restart dwm */
 	// Window
 	{ METACOMBO,                    XK_h,      			setmfact,       {.f = -0.05} },           /* master size increase */
 	{ METACOMBO,                    XK_l,      			setmfact,       {.f = +0.05} },           /* master size decrease */
-	{ METACOMBO,                    XK_bracketleft, incnmaster,     {.i = +1 } },           	/* master number increase */
-	{ METACOMBO,                    XK_bracketright,incnmaster,     {.i = -1 } },           	/* master number decrease */
+	{ METACOMBO,                    XK_bracketleft,     incnmaster,     {.i = +1 } },           	/* master number increase */
+	{ METACOMBO,                    XK_bracketright,    incnmaster,     {.i = -1 } },           	/* master number decrease */
 	// Focus/view
 	{ MODKEY,                       XK_j,      			focusstack,     {.i = +1 } },             /* stack focus heigher */
 	{ MODKEY,                       XK_k,      			focusstack,     {.i = -1 } },             /* stack focus lower */
@@ -99,12 +105,15 @@ static Key keys[] = {
 	{ MODKEY,                       XK_0,      			view,           {.ui = ~0 } },            /* view all tags */
 	{ METACOMBO,                    XK_0,      			tag,            {.ui = ~0 } },            /* tag focused to 0, making it sticky */
 	/* Layouts */
-	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[0]} },     /* layout to tile */
-	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[1]} },     /* layout to floating */
-	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[2]} },     /* layout to monocle */
-	{ METACOMBO,                    XK_f,  					setlayout,      {0} },                    /* toggle between floating and previous layout  */
-	{ MODKEY|ControlMask,           XK_f,  					togglefloating, {0} },                    /* toggle floating specifically for active client */
-	// Not classified
+	{ MODKEY,                       XK_t,      			setlayout,      {.v = &layouts[1]} },     /* layout to tile */
+	{ MODKEY,                       XK_f,      			setlayout,      {.v = &layouts[2]} },     /* layout to floating */
+	{ MODKEY,                       XK_m,      			setlayout,      {.v = &layouts[3]} },     /* layout to monocle */
+    { MODKEY,                       XK_r,               setlayout,      {.v = &layouts[4]} },     /* layout to fibonacci spiral */
+    { MODKEY|ShiftMask,             XK_r,               setlayout,      {.v = &layouts[0]} },     /* layout to fibonacci dwindle */
+
+	{ METACOMBO,                    XK_f,  				setlayout,      {0} },                    /* toggle between floating and previous layout  */
+	{ MODKEY|ControlMask,           XK_f,  				togglefloating, {0} },                    /* toggle floating specifically for active client */
+	// Multiple monitors
 	{ MODKEY,                       XK_comma,  			focusmon,       {.i = -1 } },
 	{ MODKEY,                       XK_period, 			focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  			tagmon,         {.i = -1 } },
